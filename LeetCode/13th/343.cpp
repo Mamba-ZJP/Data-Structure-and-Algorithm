622586cee9a8168006f7291113fc42684dbc3ca7
Mamba-ZJP <54902906+Mamba-ZJP@users.noreply.github.com>
2020-07-27 07:29:16 +0800

    Update 343.cpp

--- LeetCode/13th/343.cpp
+++ LeetCode/13th/343.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
-   
+    int memo[59];
+    int dfs(int n){
+        if(n <= 2) return 1;
+        if(memo[n] != -1) return memo[n];
+        int t = 1;
+//枚举所有情况 f(n)=max{i * f(n - i), i * (n-i)} (1 <= i <= n-1)
+//当我枚举了当前n的可拆分的第一个数字i的时候，整数n-i可以分成哪些数，就是一个更小的子问题了，注意在这里答案也有可能只有两个数
+        for(int i = 1; i <= n - 1; ++i){
+            t = max(t, max(i * dfs(n - i), i * (n - i)));
+        }
+        return memo[n] = t;
+    }
 
     int integerBreak(int n) {
-        if (n <= 3) return n - 1; 
-        int x = n / 3, y = n % 3; 
-        if (y == 1) return (int) pow(3, x - 1) * 4; 
-
-        if (y == 0) return (int)pow(3, x); 
-        return (int) pow(3, x) * 2;
-
+        memset(memo, -1, sizeof memo);
+        return dfs(n);
     }
 };
