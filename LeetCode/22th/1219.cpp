f1bb0beb5c4fa0da4ed80403cf7f7d147feb788d
Mamba_ZJP <54902906+Mamba-ZJP@users.noreply.github.com>
2020-08-28 14:52:02 +0800

    Update 1219.cpp

--- LeetCode/22th/1219.cpp
+++ LeetCode/22th/1219.cpp
@@ -27,12 +27,10 @@ public:
         memset(vis, 0, sizeof(vis));
         n = grid.size(), m = grid[0].size();
         this->grid = grid;
-        for (int i = 0; i < n; ++i){
+        for (int i = 0; i < n; ++i)
             for (int j = 0; j < m; ++j)
-                if (grid[i][j]) {
-                    ans = max(ans, dfs(i, j));
-                }
-        }
+                if (grid[i][j])  ans = max(ans, dfs(i, j));
+                
         return ans;
     }
 };
