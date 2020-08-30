8070fad148c317844758dabf5c35d533e9e433f3
Mamba_ZJP <54902906+Mamba-ZJP@users.noreply.github.com>
2020-08-30 10:29:18 +0800

    Update KMP.cpp

--- String/KMP.cpp
+++ String/KMP.cpp
@@ -1,11 +1,36 @@
 #include<iostream>
 #include<vector>
-using namespace std;
+#include<string>
+
+using std::vector;
+using std::string;
 
 vector<int> next;
 
-void build(const vector<int> & pattern){
-    
+void build(const string & pattern){
+    int n = pattern.length();
+    next.resize(n + 1);
+    int i = -1, j = 0; // j是主串
+    next[0] = -1;
+    while (j < n){
+        if (i == -1 || pattern[i] == pattern[j]){
+            ++i, ++j;
+            next[j] = i;
+        }
+        else i = next[i];
+    }
+}
+
+vector<int> match(const string & text, const string & pattern){
+    build(pattern);
+    int n = pattern.length();
+    vector<int> res;
+    for (int i = 0, j = 0; i < n; ++i){
+        while (j > 0 && text[i] != pattern[j]) j = next[j];
+        if (text[i] == pattern[j]) ++j;
+        if (j == n) res.push_back(i - n + 1), j = next[j];
+    }
+    return res;
 }
 
 int main(void){
