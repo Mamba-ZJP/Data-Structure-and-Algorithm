a1d2a12701cadd66a9364265d8fade25815ca2a0
Mamba-ZJP <1330472951@qq.com>
2020-09-24 21:50:24 +0800

    提交了LeetCode第28次作业。

---
+++ LeetCode/28th/62.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+        dp[1][1] = 1;
+        for (int i = 1; i <= m; i++){
+            for (int j = 1; j <= n; j++)
+                dp[i][j] += dp[i - 1][j] + dp[i][j - 1];
+        }
+        return dp[m][n];
+    }
+};
