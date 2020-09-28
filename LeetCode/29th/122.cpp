933dec2b3f27072bc0e74c287c3731d1763d1e34
Mamba-ZJP <1330472951@qq.com>
2020-09-28 08:11:51 +0800

    ..

--- LeetCode/29th/122.cpp
+++ LeetCode/29th/122.cpp
@@ -11,7 +11,7 @@ public:
         n = p.size();
         dp.resize(n, vector<int>(2, 0));
         
-        dp[0][0] = 0, dp[0][1] -= p[0];
+        dp[0][0] = 0, dp[0][1] = -p[0];
         for (int i = 1; i < n; i++){
             dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + p[i]);
             dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - p[i]);
