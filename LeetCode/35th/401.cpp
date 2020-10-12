a33c6811137e0fa668cb65006b641dd106076258
Mamba-ZJP <1330472951@qq.com>
2020-10-12 12:35:23 +0800

    Submit 35th LeetCode assignment.

---
+++ LeetCode/35th/401.cpp
@@ -0,0 +1,62 @@
+#define pub push_back
+class Solution {
+private:
+    vector<string> ans;
+    void dfs1(int i, int dep, vector<int> &hour, int cur) {
+        if (i == 0) {
+            if (cur >= 0 && cur <= 11) hour.pub(cur);
+            return;
+        }
+        if (dep > 3) return;
+        dfs1(i - 1, dep + 1, hour, cur + pow(2, dep));
+        dfs1(i, dep + 1, hour, cur);
+    }
+
+    void dfs2(int j, int dep, vector<int> &min, int cur) {
+        if (j == 0) {
+            if (cur >= 0 && cur <= 59) min.pub(cur);
+            return;
+        }
+        if (dep > 6) return;
+        dfs2(j - 1, dep + 1, min, cur + pow(2, dep));
+        dfs2(j, dep + 1, min, cur);
+    }
+public:
+    vector<string> readBinaryWatch(int num) {
+        // 上面最多3个
+        for (int i = 0; i <= min(num, 3); i++) {
+            int j = num - i;
+            if (j > 5) continue;
+            vector<int> hour, min;
+            dfs1(i, 0, hour, 0);
+            dfs2(j, 0, min, 0);
+            if (min.size() && hour.size()) {
+                for (int &h: hour) {
+                    for (int &m: min) {
+                        string t = to_string(h) + ":";
+                        if (m < 10) t += '0';
+                        t += to_string(m);
+                        ans.pub(t);
+                    }
+                }
+            } else if (min.empty()) {
+                for (int &h: hour) {
+                    string t = to_string(h);
+                    t += ":00";
+                    ans.pub(t);
+                }
+            } else if (hour.empty()) {
+                for (int &m: min) {
+                    string t = "0:";
+                    if (m < 10) t += '0';
+                    t += to_string(m);
+                    ans.pub(t);
+                }
+            } else {
+                ans.pub("0:00");
+            }
+        }
+        return ans;
+
+    }
+};
